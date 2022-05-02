dd9fe0b581e67b7aa9f05a6a74ddeb8a5b259ba6
Javiera Cortés <javieracortesceledon@gmail.com>
2022-05-02 03:33:11 -0400

    Segundo Avance

--- js/script.js
+++ js/script.js
@@ -1,36 +1,51 @@
 const words = [
-    'californication',
-    'plataforma5',
-    'black',
-    'summer',
-    'flea',
-    'aeroplane',
-    'peppers',
-    'unlimited',
-    'arcadium',
-    'love',
-    'getaway',
-    'stadium',
-    'quixoticelixer',
-    'quarter',
-    'snow',
-    'dylan',
-    'zephyr',
-    'funky',
-    'chili'
-    ];
-    
-    var palabraAleatoria;
-    var time = 10;
-    var score = 0;
+  'californication',
+  'plataforma5',
+  'black',
+  'summer',
+  'flea',
+  'aeroplane',
+  'peppers',
+  'unlimited',
+  'arcadium',
+  'love',
+  'getaway',
+  'stadium',
+  'quixoticelixer',
+  'quarter',
+  'snow',
+  'dylan',
+  'zephyr',
+  'funky',
+  'chili'
+];
 
-    function randomWords(){
-      palabraAleatoria = words[Math.floor(Math.random()*words.length)];
-      return palabraAleatoria;
-    }
+var palabraAleatoria;
+var time = 10;
+var score = 0;
+var palabraIngresada;
+palabraIngresada = document.querySelector('input');
+palabraIngresada.addEventListener('input', eventoInput);
 
-    function addToDom(){
-        document.getElementById('randomWord').textContent = randomWords();
-    }
+function randomWords() {
+  palabraAleatoria = words[Math.floor(Math.random() * words.length)];
+  return palabraAleatoria;
+}
 
-    console.log(addToDom());
+function eventoInput(e) {
+  palabraIngresada = e.target.value;
+  console.log(palabraIngresada);
+  if (palabraIngresada == palabraAleatoria) {
+    console.log("Atinaste");
+    time += 3;
+    console.log("nuevo valor time: " + time);
+    palabraIngresada = e.target.value = "";
+    console.log(addToDom())
+  }
+}
+
+function addToDom() {
+  palabraAleatoria = document.getElementById('randomWord').textContent = randomWords();
+}
+
+console.log(addToDom())
